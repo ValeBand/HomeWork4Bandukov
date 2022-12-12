8e419cee09e2f9b9c8e7d0c16d92d22190b2f267
ValeBand <119262571+ValeBand@users.noreply.github.com>
2022-12-12 17:14:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/task8/MainFragment.kt
@@ -0,0 +1,39 @@
+package com.example.task8
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bF1 = view.findViewById<Button>(R.id.bF1)
+        val bF2 = view.findViewById<Button>(R.id.bF2)
+        val controller = findNavController()
+
+        bF1.setOnClickListener {
+            controller.navigate(R.id.action_mainFragment_to_fragment1)
+        }
+
+        bF2.setOnClickListener {
+            controller.navigate(R.id.action_mainFragment_to_fragment2)
+        }
+    }
+
+}
